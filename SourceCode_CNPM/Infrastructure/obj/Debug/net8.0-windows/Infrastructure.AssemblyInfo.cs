09ac3add769f9032389fd69924f6d0b5943f2061
Tran Minh Thuyen <tranthuyen2222@gmail.com>
2025-04-22 10:48:11 +0700

    truoc khi mergr: OK

--- SourceCode_CNPM/Infrastructure/obj/Debug/net8.0-windows/Infrastructure.AssemblyInfo.cs
+++ SourceCode_CNPM/Infrastructure/obj/Debug/net8.0-windows/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e7d3468bfe290bb36c40247fb042585605a18ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9281624617f4a75190bd982efa1857c6e56f67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
